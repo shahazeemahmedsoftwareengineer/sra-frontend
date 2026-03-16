e39838152c3adfb5d1915c6279760945d2fc01b2
Conion Developer <shahazeem.ahmed.software.engineer@gmail.com>
2026-03-16 23:24:29 +0530

    fix: full-width dashboard layout + entropy animation

--- pages/_app.js
+++ pages/_app.js
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }) {
 function AuthNav() {
   const router = useRouter();
   const isLogin = router.pathname === '/login';
-
+ 
   return (
     <nav>
       <div className="nav-inner">
